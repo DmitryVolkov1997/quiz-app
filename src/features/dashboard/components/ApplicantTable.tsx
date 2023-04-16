4ed66fc82037e10a65ee5eb7537cb3f31fefcb74
dmitriy volkov <dmitrij_volkov_2014@mail.ru>
2023-04-16 14:36:17 +0600

    добавил export в excell

--- src/features/dashboard/components/ApplicantTable.tsx
+++ src/features/dashboard/components/ApplicantTable.tsx
@@ -1,105 +1,106 @@
-import {TableContainer, Tbody, Td, Th, Thead, Tr, Table} from "@chakra-ui/table"
-import React, {useMemo} from 'react'
+import { ArrowBackIcon, ArrowForwardIcon, ArrowLeftIcon, ArrowRightIcon, DeleteIcon, DownloadIcon } from "@chakra-ui/icons"
+import { Box, Button, Select } from '@chakra-ui/react'
+import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table"
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { motion } from "framer-motion"
+import { useCallback, useMemo } from 'react'
+import { Column, usePagination, useTable } from 'react-table'
+import { deleteApplicant } from 'services'
+import { Applicant, FormDataTypes } from 'types'
+import { utils, writeFileXLSX } from 'xlsx'
 import styles from "./ApplicantTable.module.sass"
-import {Applicant, FormDataTypes} from 'types'
-import {Column, usePagination, useTable} from 'react-table'
-import {Box, Button, Select} from '@chakra-ui/react'
-import {ArrowBackIcon, ArrowForwardIcon, ArrowLeftIcon, ArrowRightIcon, DeleteIcon} from "@chakra-ui/icons"
-import {useMutation, useQueryClient} from '@tanstack/react-query'
-import {deleteApplicant} from 'services'
-import {motion} from "framer-motion"
 
 interface ApplicantTableProps {
 	applicants: Applicant[]
 }
 
-export const ApplicantTable = ({applicants}: ApplicantTableProps) => {
+export const ApplicantTable = ({ applicants }: ApplicantTableProps) => {
 	const queryClient = useQueryClient()
-	const {mutateAsync} = useMutation({
+	const { mutateAsync } = useMutation({
 		mutationFn: deleteApplicant,
 		onSuccess: () => {
-			queryClient.invalidateQueries({queryKey: ["applicants"]}).then(r => console.log(r))
+			queryClient.invalidateQueries({ queryKey: ["applicants"] }).then(r => console.log(r))
 		}
 	})
 
 	const columns: readonly
 		Column<FormDataTypes & { id: string }>[] = useMemo(
-		() => [
-			{
-				Header: 'id',
-				accessor: 'id',
-			},
-			{
-				Header: 'Имя',
-				accessor: 'firstName',
-			},
-			{
-				Header: 'Фамилия',
-				accessor: 'lastName',
-			},
-			{
-				Header: 'Отчество',
-				accessor: 'patronymic',
-			},
-			{
-				Header: 'Email',
-				accessor: 'email',
-			},
-			{
-				Header: 'Телефон',
-				accessor: 'phone',
-			},
-			{
-				Header: 'Дата рождения',
-				accessor: 'birthday',
-			},
-			{
-				Header: 'Социальный статус',
-				accessor: 'status',
-			},
-			{
-				Header: 'Регион',
-				accessor: 'region',
-			},
-			{
-				Header: 'Город',
-				accessor: 'city',
-			},
-			{
-				Header: 'Вид учебного заведения',
-				accessor: 'institutionType',
-			},
-			{
-				Header: 'Учебное заведение',
-				accessor: 'educationInstitution',
-			},
-			{
-				Header: 'Форма обучения',
-				accessor: 'formStudy',
-			},
-			{
-				Header: 'Награды и достижения',
-				accessor: 'achievement',
-			},
-			{
-				Header: 'Предполагаемая форма оплаты за обучение в ВУЗе',
-				accessor: 'formPayment',
-			},
-			{
-				Header: 'Язык обучения',
-				accessor: 'language',
-			},
-			{
-				Header: 'Кафедра-консультант',
-				accessor: 'consultant',
-			},
-			{
-				Header: 'Вопрос',
-				accessor: 'question',
-			},
-		],
-		[]
-	)
+			() => [
+				{
+					Header: 'id',
+					accessor: 'id',
+				},
+				{
+					Header: 'Имя',
+					accessor: 'firstName',
+				},
+				{
+					Header: 'Фамилия',
+					accessor: 'lastName',
+				},
+				{
+					Header: 'Отчество',
+					accessor: 'patronymic',
+				},
+				{
+					Header: 'Email',
+					accessor: 'email',
+				},
+				{
+					Header: 'Телефон',
+					accessor: 'phone',
+				},
+				{
+					Header: 'Дата рождения',
+					accessor: 'birthday',
+				},
+				{
+					Header: 'Социальный статус',
+					accessor: 'status',
+				},
+				{
+					Header: 'Регион',
+					accessor: 'region',
+				},
+				{
+					Header: 'Город',
+					accessor: 'city',
+				},
+				{
+					Header: 'Вид учебного заведения',
+					accessor: 'institutionType',
+				},
+				{
+					Header: 'Учебное заведение',
+					accessor: 'educationInstitution',
+				},
+				{
+					Header: 'Форма обучения',
+					accessor: 'formStudy',
+				},
+				{
+					Header: 'Награды и достижения',
+					accessor: 'achievement',
+				},
+				{
+					Header: 'Предполагаемая форма оплаты за обучение в ВУЗе',
+					accessor: 'formPayment',
+				},
+				{
+					Header: 'Язык обучения',
+					accessor: 'language',
+				},
+				{
+					Header: 'Кафедра-консультант',
+					accessor: 'consultant',
+				},
+				{
+					Header: 'Вопрос',
+					accessor: 'question',
+				},
+			],
+			[]
+		)
 
 	const {
 		getTableProps,
@@ -116,11 +117,11 @@ export const ApplicantTable = ({applicants}: ApplicantTableProps) => {
 		nextPage,
 		previousPage,
 		setPageSize,
-		state: {pageIndex, pageSize},
+		state: { pageIndex, pageSize },
 	} = useTable({
 		columns,
 		data: applicants,
-		initialState: {pageIndex: 0, pageSize: 7},
+		initialState: { pageIndex: 0, pageSize: 7 },
 	}, usePagination)
 
 	const MotionTd = motion(Td)
@@ -130,8 +131,40 @@ export const ApplicantTable = ({applicants}: ApplicantTableProps) => {
 		mutateAsync(id).then(r => console.log(r))
 	}
 
+	const handleExportData = useCallback(() => {
+		const students = applicants.map((applicant) => {
+			return {
+				'Имя': applicant.firstName,
+				'Фамилия': applicant.lastName,
+				'Отчество': applicant.patronymic,
+				'Email': applicant.email,
+				'Телефон': applicant.phone,
+				'Дата рождения': applicant.birthday,
+				'Социальный статус': applicant.status,
+				'Регион': applicant.region,
+				'Город': applicant.city,
+				'Вид учебного заведения': applicant.institutionType,
+				'Учебное заведение': applicant.educationInstitution,
+				'Форма обучения': applicant.formStudy,
+				'Предполагаемая образовательная программа': applicant.educationalProgram,
+				'Награды и достижение': applicant.achievement,
+				'Предполагаемая форма оплаты за обучение в ВУЗе': applicant.formPayment,
+				'Язык обучения': applicant.language,
+				'Кафедра-консультант - от кого получена информация о ВУЗе)': applicant.consultant,
+				'Вопрос': applicant.question
+			}
+		})
+		const ws = utils.json_to_sheet(students)
+		const wb = utils.book_new()
+		utils.book_append_sheet(wb, ws, 'Data')
+		writeFileXLSX(wb, 'SheetJSReactAoO.xlsx')
+	}, [applicants])
+
 	return (
-		<TableContainer boxShadow="2xl" rounded="md">
+		<TableContainer
+			boxShadow="2xl"
+			rounded="md"
+		>
 			<Table {...getTableProps()}>
 
 				<Thead>
@@ -165,16 +198,16 @@ export const ApplicantTable = ({applicants}: ApplicantTableProps) => {
 						return (
 							<MotionTr
 								{...row.getRowProps()}
-								initial={{opacity: 0, y: -10}}
-								animate={{opacity: 1, y: 0}}
-								transition={{duration: 0.5}}
+								initial={{ opacity: 0, y: -10 }}
+								animate={{ opacity: 1, y: 0 }}
+								transition={{ duration: 0.5 }}
 							>
 								{row.cells.map((cell, index) => {
 									return (
 										<MotionTd {...cell.getCellProps()}
-												  initial={{opacity: 0, x: -10}}
-												  animate={{opacity: 1, x: 0}}
-												  transition={{duration: 0.5}}
+											initial={{ opacity: 0, x: -10 }}
+											animate={{ opacity: 1, x: 0 }}
+											transition={{ duration: 0.5 }}
 										>
 											{index === 0 ? (
 												<Button
@@ -184,7 +217,7 @@ export const ApplicantTable = ({applicants}: ApplicantTableProps) => {
 													h={8}
 													mr={4}
 												>
-													<DeleteIcon fontSize="md"/>
+													<DeleteIcon fontSize="md" />
 												</Button>
 											) : cell.render('Cell')}
 										</MotionTd>
@@ -201,7 +234,9 @@ export const ApplicantTable = ({applicants}: ApplicantTableProps) => {
 			<Box
 				className={styles.pagination}
 			>
-				<Select value={pageSize} onChange={(e) => setPageSize(Number(e.target.value))}>
+				<Select
+					value={pageSize}
+					onChange={(e) => setPageSize(Number(e.target.value))}>
 					{[10, 20, 30, 40, 50].map((pageSize) => (
 						<option key={pageSize} value={pageSize}>
 							Показать {pageSize}
@@ -215,21 +250,21 @@ export const ApplicantTable = ({applicants}: ApplicantTableProps) => {
 						onClick={() => gotoPage(0)} disabled={!canPreviousPage}
 						background={"transparent"}
 					>
-						<ArrowLeftIcon fontSize="md"/>
+						<ArrowLeftIcon fontSize="md" />
 					</Button>
 
 					<Button
 						onClick={() => previousPage()} disabled={!canPreviousPage}
 						background={"transparent"}
 					>
-						<ArrowBackIcon fontSize="2xl"/>
+						<ArrowBackIcon fontSize="2xl" />
 					</Button>
 
 					<Button
 						onClick={() => nextPage()} disabled={!canNextPage}
 						background={"transparent"}
 					>
-						<ArrowForwardIcon fontSize="2xl"/>
+						<ArrowForwardIcon fontSize="2xl" />
 					</Button>
 
 					<Button
@@ -237,21 +272,31 @@ export const ApplicantTable = ({applicants}: ApplicantTableProps) => {
 						disabled={!canNextPage}
 						background={"transparent"}
 					>
-						<ArrowRightIcon fontSize="md"/>
+						<ArrowRightIcon fontSize="md" />
 					</Button>
 
 				</Box>
 
-				<Box className={styles.pageCount} as="span">
+				<Box
+					className={styles.pageCount}
+					as="span"
+				>
 					Страница&nbsp;
 
 					<Box as="strong">
 						{pageIndex + 1} из {pageOptions.length}
 					</Box>
+
+					<Button
+						colorScheme='facebook'
+						ml={10}
+					>
+						<DownloadIcon onClick={handleExportData} />
+					</Button>
 				</Box>
 
 			</Box>
-		</TableContainer>
+		</TableContainer >
 	)
 }
 
